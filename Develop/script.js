140510160efc59af3edac3d973b35656d0c81018
William Gartman <wngartman@gmail.com>
2021-12-23 18:00:02 -0600

    Fixed the real bug so now it gives you a second chance if you choose a wrong nubmer

--- Develop/script.js
+++ Develop/script.js
@@ -15,11 +15,14 @@ function generatePassword() {
 
   var length = prompt("How many characters? (Must be between 8-128)");
 
-  if (length >= 128 && length < 8) {
+  if (length >= 128 || length < 8) {
     alert("Length must be greater than 8 and less than 128 Characters")
+    var length = prompt("How many characters? (Must be between 8-128)");
   } else {
-    alert("Perfect! " + length + " works!")
-  };
+      alert("Perfect! " + length + " works!")
+    }
+
+
 
   var confirmLower = confirm("Would you like to add lowercase letters to your password?")
   var confirmCapital = confirm("Would you like to add capital letters to your password?")
